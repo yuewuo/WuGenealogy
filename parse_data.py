c66b329022fd146bd8bfb9c4bcc657b7484f83a0
Yue Wu <yue.wu@yale.edu>
2025-06-20 20:50:54 -0400

    add wenling yan

--- parse_data.py
+++ parse_data.py
@@ -1,33 +1,39 @@
-"""
-
-"""
+""" """
 
 import csv, json
+
 
 def main():
     parse_file("data.csv", "data.json")
 
-NONE_NAMES = { "", "(无嗣)" }
+
+NONE_NAMES = {"", "(无嗣)"}
+
+
 def is_not_name(name):
-    return (name in NONE_NAMES)
+    return name in NONE_NAMES
+
+
 def is_some_name(name):
     return not is_not_name(name)
+
 
 def build_tree(table, i, j):
     root = table[i][j]
     children = []
     height = len(table)
     width = len(table[0])
-    if j + 1 < width and is_some_name(table[i][j+1]):
+    if j + 1 < width and is_some_name(table[i][j + 1]):
         # try to add children
-        children.append(build_tree(table, i, j+1))
-        for ic in range(i+1, height):
-            if is_some_name(table[ic][j+1]):
+        children.append(build_tree(table, i, j + 1))
+        for ic in range(i + 1, height):
+            if is_some_name(table[ic][j + 1]):
                 if is_not_name(table[ic][j]):
-                    children.append(build_tree(table, ic, j+1))
+                    children.append(build_tree(table, ic, j + 1))
                 else:
                     break
     return (root, children)
+
 
 def parse_file(input_csv, output_json):
     table = []
@@ -38,6 +44,27 @@ def parse_file(input_csv, output_json):
     tree = build_tree(table, 1, 0)
     with open(output_json, "w", encoding="utf-8") as f:
         f.write(json.dumps(tree, ensure_ascii=False))
+
+    # also attempt to update the index.html file
+    found_const_origin_data = False
+    with open("index.html", "r", encoding="utf-8") as f:
+        content = ""
+        for line in f:
+            line = line.rstrip("\r\n ")
+            if line.startswith("const origin_data = "):
+                assert not found_const_origin_data, "duplicate const origin_data"
+                found_const_origin_data = True
+                content += (
+                    "const origin_data = "
+                    + json.dumps(tree, ensure_ascii=False)
+                    + ";\n"
+                )
+            else:
+                content += line + "\n"
+    if found_const_origin_data:
+        with open("index.html", "w", encoding="utf-8") as f:
+            f.write(content)
+
 
 if __name__ == "__main__":
     main()
